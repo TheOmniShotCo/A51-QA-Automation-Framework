9c67e12b893bf8ae60fed1d12bbf4d34d782c302
TheOmniShotCo <aleks@theomnishot.com>
2023-11-03 17:53:25 -0500

    homework20
    homework20

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,67 +1,73 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
+
 import java.time.Duration;
 
-
 public class BaseTest {
+
     public WebDriver driver = null;
-    // String url = "https://qa.koel.app/";
-    //driver.get(url);
+    public WebDriverWait wait = null;
+    public static Actions actions = null;
+    public  String url = "https://qa.koel.app/";
 
-    public String url;
-    // private String baseURL;
-
-    @BeforeSuite
+    @BeforeSuite //The annotated method will run before all tests in this suit have run
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeMethod
+    @BeforeMethod //The annotated method will be run before all the test methods in the current class have been run.
     @Parameters({"BaseURL"})
-    public void launchBrowser(String baseURL) {
-        // Added ChromeOptions argument below to fix websocket error
+    public void  launchBrowser(String BaseURL){
+        //Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--start-maximized");
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        url = baseURL;
+        driver.manage().window().maximize();
+
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        url = BaseURL;
         navigateToPage();
-    }
 
-    @AfterMethod
-    public void closeBrowser() {
+    }
+    @AfterMethod //The annotated method will be run after each test method
+    public void closeBrowser(){
         driver.quit();
-
     }
-    public void navigateToPage() {
+
+    //region Helper Methods
+    protected void navigateToPage() {
         driver.get(url);
     }
 
-    public void provideEmail(String email){
-        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    protected void provideEmail(String email) {
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));//driver.findElement(By.cssSelector("input[type='email']"));
         emailField.clear();
         emailField.sendKeys(email);
     }
 
-    public void providePassword(String password){
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+    protected void providePassword(String password) {
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));//driver.findElement(By.cssSelector("input[type='password']"));
         passwordField.clear();
         passwordField.sendKeys(password);
     }
 
-    public void clickSubmit () {
-        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-        submit.click();
+    protected void clickSubmit() {
+        WebElement submitBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']"))); //driver.findElement(By.cssSelector("button[type='submit']"));
+        submitBtn.click();
     }
+    //endregion
 }
---
+++ src/test/java/Homework20.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework20 extends BaseTest {
+
+    @Test
+    public void deletePlaylist() throws InterruptedException{
+
+        //String uniquePlaylist = "1-HW19-Playlist";
+        String deletePlaylistMsg = "Deleted playlist \"1-HW19-Playlist.\"";
+
+        //Pre-condition
+        //Login
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+
+        //Click on empty playlist created
+        clickOnPlaylist();
+
+        //delete selected playlist
+        clickOnDeleteBtn();
+
+        //Expected Results
+        WebElement notificationMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.alertify-logs div.success")));//driver.findElement(By.cssSelector("div.alertify-logs div.success"));
+
+        Assert.assertEquals(deletePlaylistMsg,notificationMsg.getText());
+
+    }
+
+    public void clickOnPlaylist(){
+        WebElement playList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));//driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        playList.click();
+    }
+
+    public void clickOnDeleteBtn(){
+        WebElement deleteBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='del btn-delete-playlist']")));//driver.findElement(By.xpath("//button[@class='del btn-delete-playlist']"));
+        deleteBtn.click();
+    }
+}
