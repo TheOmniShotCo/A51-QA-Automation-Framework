2253b9220a072909cca57787e03b919870bdc8a1
TheOmniShotCo <aleks@theomnishot.com>
2023-11-06 20:17:14 -0600

    test
    test

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Homework21 extends BaseTest {
+public class Homework21 extends BaseTest{
 
     String newPlaylistName = "HW 21 - Sample Edited Playlist";
 
@@ -13,7 +13,7 @@ public class Homework21 extends BaseTest {
     @Test
     public void renamePlaylist() throws InterruptedException {
         //Prerequisite at least one user created playlist
-        String updatedPlaylistMsg = "Updated playlist \"GabonHW 21 - Sample Edited Playlist.\"";
+        String updatedPlaylistMsg = "Updated playlist \"HW 21 - Sample Edited Playlist.\"";
 
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
@@ -33,7 +33,7 @@ public class Homework21 extends BaseTest {
         WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
         //clear() does not work, element has an attribute of "required"
         //workaround is ctrl A (to select all) then backspace to clear then replace with new playlist name
-        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL, "A", Keys.BACK_SPACE));
+        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL,"A",Keys.BACK_SPACE));
 
         playlistInputField.sendKeys(newPlaylistName);
         playlistInputField.sendKeys(Keys.ENTER);
@@ -43,4 +43,6 @@ public class Homework21 extends BaseTest {
         WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
         return notification.getText();
     }
+
+
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -7,6 +9,24 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
+
+    //region Tests
+    @Test
+    public void loginValidEmailPassword(){
+
+        navigateToPage();
+
+        //Steps
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+
+        //Expected Result
+        WebElement avatarImg = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatarImg.isDisplayed());
+
+    }
+
     @Test
     public void loginEmptyEmailPassword() {
 
@@ -21,6 +41,7 @@ public class LoginTests extends BaseTest {
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
-
     }
+    //endregion
+
 }
